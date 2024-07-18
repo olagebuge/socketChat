edbea780a4f8d5320abe49637c7dd12c6f6fb4d1
olagebuge <53190225+olagebuge@users.noreply.github.com>
2024-07-18 18:38:39 +0800

    first upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# Socket.IO即時聊天室
+
+此為socket.io製作聊天室的實例
+
+## 啟動方式
+
+devlopment
+```
+npm run dev
+或
+nodemon index.js
+```
+production
+```
+npm start
+或
+node index.js
+```
+
---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+const port = 8080;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const server = app.listen(port, () => {
+  console.log(`後端伺服器聆聽在 port ${port}...`);
+});
+
+const { Server } = require("socket.io");
+const io = new Server(server, { cors: { origin: "*" } });
+
+let NICK_NAME = ["賓士貓", "虎斑貓", "橘貓", "英國短毛貓", "矮腳貓"];
+let onlineUsers = [];
+
+io.on("connection", (socket) => {
+  if (NICK_NAME.length > 0) {
+    const randomIndex = Math.floor(Math.random() * NICK_NAME.length);
+    const nickName = NICK_NAME.splice(randomIndex, 1)[0];
+
+    onlineUsers.push({ id: socket.id, nickName });
+
+    console.log(`使用者 ${socket.id} (${nickName}) 成功連結`);
+    socket.emit("serverMessage", `使用者 ${nickName} 前來參加世界好貓咪大賞`);
+
+    // 广播更新后的在线用户列表
+    io.emit("updateUserList", onlineUsers);
+
+    socket.on("clientMessage", (message) => {     
+      socket.broadcast.emit("serverMessage", `${message.nickName}: ${message.text}`);
+    });
+
+    socket.on("disconnect", () => {
+      console.log(`使用者 ${socket.id} (${nickName}) 離開`);
+      socket.broadcast.emit("serverMessage", `使用者 ${nickName} 離開`);
+      NICK_NAME.push(nickName);
+      onlineUsers = onlineUsers.filter(user => user.id !== socket.id);
+
+      // 广播更新后的在线用户列表
+      io.emit("updateUserList", onlineUsers);
+    });
+  } else {
+    socket.emit("serverMessage", "目前暱稱已經用完");
+    socket.disconnect();
+  }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
